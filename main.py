89efdbe2d8d9d771a0587c6299ab5884c1c07a8e
vladimir <vovka1998@gmail.com>
2024-08-23 15:35:25 +0300

    cleaning main.py

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
-import configparser
-
-
-def get_key_OAuth() -> str:
-    conf = configparser.ConfigParser()
-    conf.read('config.ini')
-    key = conf.get("API", 'kay_OAuth')
-    return key
-
-
 if __name__ == '__main__':
-    get_key_OAuth()
+    pass
