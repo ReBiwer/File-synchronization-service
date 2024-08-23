0bbdc77fa6244eabcaa2ff5883c18c8efbe9abb7
vladimir <vovka1998@gmail.com>
2024-08-23 15:00:19 +0300

    add config.ini and key_OAuth

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+import configparser
+
+
+def get_key_OAuth() -> str:
+    conf = configparser.ConfigParser()
+    conf.read('config.ini')
+    key = conf.get("API", 'kay_OAuth')
+    return key
+
+
 if __name__ == '__main__':
-    pass
+    get_key_OAuth()
