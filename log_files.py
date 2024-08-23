e74676144639d8f532fc1b50e4bfe123974e0548
vladimir <vovka1998@gmail.com>
2024-08-23 14:13:21 +0300

    add funcs for create configuration logger

--- log_files.py
+++ log_files.py
@@ -2,34 +2,43 @@ import logging.config
 
 import os
 
-test_path = os.path.join(
-    os.getcwd(), 'test_log_file'
-)
 
-dict_config = {
+def _get_path_to_log() -> str:
+    return os.path.join(
+        os.getcwd(), 'test_log_file'
+    )
+
+
+def _get_dict_config() -> dict:
+    dict_config = {
         "version": 1,
         "disable_existing_loggers": True,
         "formatters": {
-            "simple": {
+            "base_format": {
                 "format": f"%(levelname)s | %(asctime)s | %(lineno)s | %(funcName)s | %(message)s",
             },
         },
         "handlers": {
-            "menu_handler": {
+            "base_handler": {
                 "class": "logging.handlers.TimedRotatingFileHandler",
                 "when": "h",
                 "interval": 10,
                 "backupCount": 5,
                 "level": "DEBUG",
-                "formatter": "menu_format",
-                "filename": test_path,
+                "formatter": "base_format",
+                "filename": _get_path_to_log(),
                 "encoding": "utf-8",
             },
         },
         "loggers": {
             'base_logger': {
                 "level": "DEBUG",
-                "handlers": ["base_handler", 'check_work', 'error_handler']
+                "handlers": ["base_handler"]
             },
         },
     }
+    return dict_config
+
+
+def config_logger() -> None:
+    logging.config.dictConfig(_get_dict_config())
