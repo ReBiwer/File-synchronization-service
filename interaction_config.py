618237a6099eaf5f18b8205acb122dcf163cc412
vladimir <vovka1998@gmail.com>
2024-08-24 09:24:50 +0300

    add interaction get dirs from config.ini

--- interaction_config.py
+++ interaction_config.py
@@ -19,3 +19,13 @@ def get_keyOAuth() -> str:
 def get_name_log_file() -> str:
     config = _get_ConfigParser()
     return config.get('logger', 'name_log_file')
+
+
+def get_name_cloud_dir() -> str:
+    config = _get_ConfigParser()
+    return config.get('dirs', 'name_cloud_dir')
+
+
+def get_name_local_dir() -> str:
+    config = _get_ConfigParser()
+    return config.get('dirs', 'name_local_dir')
