5dfb0b6bb060a460fa373a43c265b17c7043a8ba
vladimir <vovka1998@gmail.com>
2024-08-25 20:23:41 +0300

    add docstring and type return in method get_meta_info_cloud_dir

--- yandex_disk.py
+++ yandex_disk.py
@@ -1,5 +1,7 @@
 import requests
 import json
+
+from types_project.type_response import ResponseMetaDataYandexDisk
 from interaction_config import Config
 
 
@@ -10,7 +12,8 @@ class CloudDisk:
     def __init__(self):
         self.__config = Config()
 
-    def get_meta_info_cloud_dir(self):
+    def get_meta_info_cloud_dir(self) -> ResponseMetaDataYandexDisk:
+        """Метод возвращает информацию о мета информации с диска"""
         response = requests.get(
             self.__base_url_request,
             headers={'Authorization': self.__config.token_API_cloud}
