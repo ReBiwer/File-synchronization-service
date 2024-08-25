121b2facafdb4ec677879e7943e20fb684d00d3c
vladimir <vovka1998@gmail.com>
2024-08-25 12:19:00 +0300

    add method get_meta_info_cloud_dir

--- yandex_disk.py
+++ yandex_disk.py
@@ -1,9 +1,18 @@
 import requests
+import json
 from interaction_config import Config
 
 
 class CloudDisk:
     """Класс для взаимодействия с облачным диском"""
+    __base_url_request = 'https://cloud-api.yandex.net/v1/disk/'
 
     def __init__(self):
         self.__config = Config()
+
+    def get_meta_info_cloud_dir(self):
+        response = requests.get(
+            self.__base_url_request,
+            headers={'Authorization': self.__config.token_API_cloud}
+        )
+        return json.loads(response.content)
