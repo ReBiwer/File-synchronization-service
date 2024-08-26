95973078b0c401946ca484a960c730231df25dd6
vladimir <vovka1998@gmail.com>
2024-08-26 21:22:26 +0300

    add import in __init__ files

--- settings_project/__init__.py
+++ settings_project/__init__.py
@@ -1,2 +1,1 @@
-from . import config, logging_project, tests, types_project
-from .working_directories import cloud_dir
+from . import config, logging_project, tests, types_project, working_directories
