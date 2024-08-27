a19f9f16bcde0688a94f2cd25689fd5cf8e6106b
vladimir <vovka1998@gmail.com>
2024-08-27 11:34:44 +0300

    add method for crete info about file

--- settings_project/working_directories/local_disk.py
+++ settings_project/working_directories/local_disk.py
@@ -1,6 +1,28 @@
 import os
+from settings_project.types_project.type_data_dir import InfoFile
+from datetime import datetime
+from settings_project.config.interaction_config import Config
 
 
 class LocalDir:
-    """"""
-    pass
+    """Класс для взаимодействия с локальной директорией"""
+    def __init__(self):
+        self.__config = Config()
+        self.__path_local_dir = self.__config.local_dir
+
+    @staticmethod
+    def __get_time_created(path_file: str) -> datetime:
+        unix_time = os.path.getctime(path_file)
+        return datetime.fromtimestamp(unix_time)
+
+    @staticmethod
+    def __get_time_modified(path_file: str) -> datetime:
+        unix_time = os.path.getmtime(path_file)
+        return datetime.fromtimestamp(unix_time)
+
+    def __get_info_file(self, path_file: str) -> InfoFile:
+        info = InfoFile(
+            self.__get_time_created(path_file),
+            self.__get_time_modified(path_file)
+        )
+        return info
