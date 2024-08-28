7d8b6e344c13b2cfb97ea23dc8f800fd759a357e
vladimir <vovka1998@gmail.com>
2024-08-28 08:52:15 +0300

    rename attribute path_file -> name_file

--- settings_project/types_project/type_data_dir.py
+++ settings_project/types_project/type_data_dir.py
@@ -3,13 +3,13 @@ from datetime import datetime
 
 
 class InfoFile(NamedTuple):
-    path_file: str
+    name_file: str
     created_time: datetime
     modified_time: datetime
 
     def __str__(self):
         return (
-            f'Путь к файлу: {self.path_file}, '
+            f'Путь к файлу: {self.name_file}, '
             f'создан: {self.created_time}, '
             f'модифицирован: {self.modified_time}'
         )
