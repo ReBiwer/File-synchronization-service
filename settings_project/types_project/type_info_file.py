b3806f5e2ce98004889aaabb96b9373585c26796
vladimir <vovka1998@gmail.com>
2024-08-29 22:31:14 +0300

    add path_file in InfoFile

--- settings_project/types_project/type_info_file.py
+++ settings_project/types_project/type_info_file.py
@@ -4,12 +4,14 @@ from datetime import datetime
 
 class InfoFile(NamedTuple):
     name_file: str
+    path_file: str
     created_time: datetime
     modified_time: datetime
 
     def __str__(self):
         return (
-            f'Путь к файлу: {self.name_file}, '
+            f'Имя файла: {self.name_file},'
+            f'путь к файлу: {self.path_file}, '
             f'создан: {self.created_time}, '
             f'модифицирован: {self.modified_time}'
         )
