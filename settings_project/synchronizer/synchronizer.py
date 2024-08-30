08529a56a7d92e44af5ca059550001ed21894370
vladimir <vovka1998@gmail.com>
2024-08-30 14:56:05 +0300

    create Synchronizer

--- settings_project/synchronizer/synchronizer.py
+++ settings_project/synchronizer/synchronizer.py
@@ -3,9 +3,9 @@ from settings_project.working_directories.local_disk import LocalDir
 
 
 class Synchronizer:
-    def __init__(self):
-        self.__cloud_dir = CloudDir()
-        self.__local_dir = LocalDir()
+    def __init__(self, cloud: CloudDir, local: LocalDir):
+        self.__cloud_dir = cloud
+        self.__local_dir = local
 
     def check_updates(self):
         pass
