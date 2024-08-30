ae680704ecae0e0077437904d933cc8e20a86468
vladimir <vovka1998@gmail.com>
2024-08-30 14:32:53 +0300

    added docstrings

--- settings_project/working_directories/cloud_dir.py
+++ settings_project/working_directories/cloud_dir.py
@@ -16,17 +16,25 @@ class CloudDir:
         self.__cloud_dir = cloud_dir
 
     def get_info_cloud_disk(self) -> tuple[dict, int]:
-        """Метод возвращает информацию об диске"""
+        """
+        Метод возвращает информацию об диске
+        :return tuple[dict, int]: где
+        dict - словарь с информацией о диске
+        int - статус ответа
+        """
         response = requests.get(
             self.__base_url_request,
             headers=self.__header,
         )
-        type_status_code = response.status_code
-        type_info = json.loads(response.content)
-        return type_info, type_status_code
+        status_code = response.status_code
+        info_disk = json.loads(response.content)
+        return info_disk, status_code
 
     def __get_items_in_cloud_dir(self) -> list:
-        """Метод возвращает список содержимого в облачной папке"""
+        """
+        Метод возвращает список с файлами в облачной папке
+        :return list: список с подробной информацией о файлах по шаблону из АПИ
+        """
         response = requests.get(
             self.__base_url_request + 'resources/?' + f'path={self.__cloud_dir}',
             headers=self.__header,
@@ -35,6 +43,11 @@ class CloudDir:
         return res
 
     def __serializer_info_file(self, info_with_dict: dict) -> InfoFile:
+        """
+        Метод переводит всю нужную информацию о файле в единую структуру данных
+        :param info_with_dict: словарь с информацией о файле из АПИ
+        :return InfoFile: структура данных используемая в проекте
+        """
         name_file = info_with_dict['name']
         path_file = self.__cloud_dir + name_file
         created_time = datetime.strptime(info_with_dict['created'], "%Y-%m-%dT%H:%M:%S%z")
@@ -42,6 +55,10 @@ class CloudDir:
         return InfoFile(name_file, path_file, created_time, modified_time)
 
     def get_info_dir(self) -> list[InfoFile]:
+        """
+        Метод для сбора информации о файлах в облаке
+        :return list[InfoFile]: список с информацией о каждом файле преобразованная к стандарту проекта
+        """
         info_about_files = list()
         for info_file in self.__get_items_in_cloud_dir():
             info_file = self.__serializer_info_file(info_file)
@@ -49,6 +66,11 @@ class CloudDir:
         return info_about_files
 
     def __get_url_load(self, info_file: InfoFile) -> str:
+        """
+        Метод для получения ссылки на загрузку файла
+        :param info_file: путь к новому файле на диске (не путать с путем к файлу на локальной машине)
+        :return str: URL для отправки файла на облако
+        """
         url = self.__base_url_request + f'resources/upload?path=/{self.__cloud_dir}/{info_file.name_file}'
         request = requests.get(
             url=url,
@@ -57,19 +79,38 @@ class CloudDir:
         return request.json()['href']
 
     def load_file(self, info_file_cloud: InfoFile, downloadable_file: BinaryIO) -> int:
+        """
+        Метод для загрузки файла
+        :param info_file_cloud: информация о файле в облаке
+        :param downloadable_file: файл, который нужно загрузить в облако в бинарном формате
+        :return int: возвращает статус код запроса на облако
+        """
         url_upload_file = self.__get_url_load(info_file_cloud)
         response = requests.put(url_upload_file, headers=self.__header, data=downloadable_file)
         return response.status_code
 
     def delete_file(self, info_file_cloud: InfoFile) -> int:
+        """
+        Метод для удаления файла в облаке
+        :param info_file_cloud: информация о файле в облаке
+        :return int: возвращает статус код запроса на облако
+        """
         url = self.__base_url_request + f'resources?path=/{self.__cloud_dir}/{info_file_cloud.name_file}'
         response = requests.delete(url, headers=self.__header)
         return response.status_code
 
-    def reload_file(self, info_file_cloud: InfoFile, reloadable_file: BinaryIO) -> tuple[int, int]:
-        status_delete = self.delete_file(info_file_cloud)
+    def reload_file(self, info_file_cloud: InfoFile, reloadable_file: BinaryIO) ->  int:
+        """
+        Метод для обновления файла в облаке
+        (поскольку в АПИ Яндекса нет отдельного метода для обновления файла,
+        пришлось использовать такой способ)
+        :param info_file_cloud: информация о файле в облаке
+        :param reloadable_file: файл, который нужно загрузить в облако в бинарном формате
+        :return int: возвращает статус код запроса на облако
+        """
+        self.delete_file(info_file_cloud)
         status_load = self.load_file(info_file_cloud, reloadable_file)
-        return status_delete, status_load
+        return status_load
 
 
 if __name__ == '__main__':
