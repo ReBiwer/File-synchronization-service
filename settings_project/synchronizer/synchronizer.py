53bc8fab9a8c5b30b8a8ee32959ec12768008233
vladimir <vovka1998@gmail.com>
2024-08-31 16:15:58 +0300

    delete staticmethod

--- settings_project/synchronizer/synchronizer.py
+++ settings_project/synchronizer/synchronizer.py
@@ -9,19 +9,12 @@ class Synchronizer:
         self.__cloud_dir = cloud
         self.__local_dir = local
 
-    @staticmethod
-    def __check_file_in_dir(name_file: str, info_cloud_dir: list[InfoFile]) -> bool:
-        for info_file_cloud in info_cloud_dir:
-            if info_file_cloud.name_file == name_file:
-                return True
-        return False
-
     def check_updates_files(self):
         info_local_dir = self.__local_dir.get_info_dir()
         info_cloud_dir = self.__cloud_dir.get_info_dir()
         for info_file_local in info_local_dir:
             #  Проверка наличия файла в облаке. Если его нет, то загрузить
-            if not self.__check_file_in_dir(info_file_local.name_file, info_cloud_dir):
+            if not (info_local_dir in info_cloud_dir):
                 with open(info_file_local.path_file, 'rb') as downloadable_file:
                     self.__cloud_dir.load_file(info_file_local, downloadable_file)
 
