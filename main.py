2fa9cffd14bc7399f931b6a8821e69c554e0856e
vladimir <vovka1998@gmail.com>
2024-09-01 16:44:49 +0300

    adding working project

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+import time
+from settings_project.synchronizer.synchronizer import Synchronizer
+from settings_project.config.interaction_config import Config
+from settings_project.working_directories.cloud_dir import CloudDir
+from settings_project.working_directories.local_dir import LocalDir
+
+
 if __name__ == '__main__':
-    pass
+    conf = Config()
+    local_dir = LocalDir(conf.local_dir)
+    cloud_dir = CloudDir(conf.token_API_cloud, conf.cloud_dir)
+    sync = Synchronizer(cloud_dir, local_dir)
+    while True:
+        sync.check_files()
+        time.sleep(conf.time_synchronization)
